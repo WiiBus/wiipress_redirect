2a2bca4524c260f85bd4dcd9c6940cdac8f39f79
Rodrigo Ramos <rodrigoestevesramos@gmail.com>
2022-03-30 15:31:17 +0200

    Fork project

---
+++ script.js
@@ -0,0 +1,36 @@
+/*
+    Développé par l'équipe technique de Wiibus
+    Date de création: 2022
+    Type de fichier: JS
+*/
+
+(function() {
+    var url = new URL(window.location.href);
+    let company = url.searchParams.get("company");
+    if(company !== null) {
+
+        // NOTE Créer une image temporaire pour vérifier si elle existe dans le dossier "./company"
+        const companyImage = new Image();
+        companyImage.src = './company/'+company+".png";
+        
+        // L'image existe
+        companyImage.onload = () => {
+            document.getElementById('main').style.display = "block";
+            document.getElementById('company_logo').src = companyImage.src;
+        };
+        
+        // Image inéxistante
+        companyImage.onerror = async () => {
+            // Vérification avec différents extensions
+            if(companyImage.src.includes('.png')) {
+                companyImage.src = './company/'+company+".jpg";
+            } else if (companyImage.src.includes('.jpg')) {
+                companyImage.src = './company/'+company+".jpeg";
+            } else if(companyImage.src.includes('.jpeg')) {
+                companyImage.src = './company/'+company+".svg";
+            } else {
+                return;
+            }
+        };
+    }
+ })();
